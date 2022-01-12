a2de4d151bd671451ab67a28c6c39506f3acc32e
Zhaokai Guan <zguan25@myseneca.ca>
2022-01-12 11:35:37 -0500

    Submitting Workshop Zero

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OOP244Works
+- Name: ZHaokai Guan
+- Student Number: 130988215
+- Seneca E-mail: zguan25@myseneca.ca
---
+++ WorkshopZero/prg.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 0; i < 10; i++){
+		cout << "Hello I am Zhaokai Guan" << endl;
+	}
+	return 0;
+}
