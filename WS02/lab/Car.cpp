9c23898af10bbf6179e19b5d8d73745fb23feab9
Zhaokai Guan <zguan25@myseneca.ca>
2022-01-26 16:29:46 -0500

    First draft. Resolved the crushing bugs... Finally... Jan26

--- WS02/lab/Car.cpp
+++ WS02/lab/Car.cpp
@@ -1,12 +1,13 @@
 #include"cStrTools.h"
 #include "Car.h"
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 
 namespace sdds {
-    int car_Array_size = 2;
-    int allocation_Size = 3;
+    int car_Array_size = 0;
+    int allocation_Size = 0;
     Car* car_Ptr = nullptr;
     int car_Num = 0;
     
@@ -24,30 +25,63 @@ namespace sdds {
         car_Array_size = allocSize;
         car_Ptr = new Car[allocation_Size];
     }
-    void deallocate(Car& C) {
-        
 
-
-        delete[]car_Ptr->make_Model;
-        car_Ptr->make_Model = nullptr;
-
+    void deallocate(Car& C) {     
+        delete[] C.make_Model;
+        C.make_Model = nullptr;
     }
 
     bool read(Car& C) {
         bool flag = false;
         char temp_string_holder[61]={0};
        
-        // problem might come from this line, overloading??? right???///
-        read(temp_string_holder, 60, ',');
-        if (!(strCmp(temp_string_holder, "X"))) {
+        read(temp_string_holder, 61, ',');
+        if (strCmp(temp_string_holder, "X")) {
+            char time_string[5] = { 0 };
             flag = true;
-            C.make_Model = new char[allocation_Size];
-            C.make_Model = temp_string_holder;
-            // might be extra, shall be tested by the debugger//
-            C.make_Model[strLen(temp_string_holder) + 1] = '\0';
+            // Missing that 1 extra space caused 2 hours debug for me//
+            C.make_Model = new char[strLen(temp_string_holder)+1];
+            strCpy(C.make_Model, temp_string_holder);
             read(C.license_Plate, 9, ',');
-           
+            read(time_string, 5);
+            // There could be better way//
+            stringstream intTime(time_string);
+            intTime >> C.time;
         }
 
+        else{
+            flag = false;
+        }
+        return flag;
     }
+
+    void print(const Car& C) {
+        cout << C.time << ": " << C.make_Model << "," << C.license_Plate << endl;
+    }
+
+    void record(const Car& C) {
+        if (car_Num==car_Array_size){
+            Car* temp = new Car[allocation_Size + car_Array_size];
+            for (int i = 0; i < car_Array_size; i++){
+                temp[i] = car_Ptr[i];
+            }
+            delete[]car_Ptr;
+            car_Ptr = temp;
+            car_Array_size += allocation_Size;
+
+        }
+        car_Ptr[car_Num] = C;
+        car_Num++;
+    }
+    
+    void endOfDay() {
+        for (int i = 0; i < car_Num; i++){
+            print(car_Ptr[i]);
+            deallocate(car_Ptr[i]);
+        }
+        delete[]car_Ptr;
+        car_Ptr = nullptr;
+    }
+
+
 }
--- WS02/lab/Car.h
+++ WS02/lab/Car.h
@@ -2,8 +2,10 @@
 #define SDDS_CAR
 namespace sdds {
 
+	 const int license_Plate_size = 9;
+
 	 struct Car{
-		  char license_Plate[9];
+		  char license_Plate[license_Plate_size];
 		  char* make_Model;
 		  int time;
 	 };
@@ -18,8 +20,12 @@ namespace sdds {
 	 void deallocate(Car& C);
 	 // Overloaded function to read the info from the input buffer DMA involved//
 	 bool read(Car& C);
-
-
+	 // print out the car in a format//;
+	 void print(const Car& C);
+	 // Adding the info into the Car arr//
+	 void record(const Car& C);
+	 // Print and deallocate each member from the Car arr//
+	 void endOfDay();
 }
 
 #endif // !SDDS_CAR_H
