59b3f8f4d327c5a0b234553b71a496491ef2eff9
Zhaokai Guan <zguan25@myseneca.ca>
2022-02-02 13:45:22 -0500

    Feb2nd Needs a final check
    Signed-off-by: Zhaokai Guan <zguan25@myseneca.ca>

---
+++ WS03/lab/Train.cpp
@@ -0,0 +1,85 @@
+/* ------------------------------------------------------
+I have done all the coding by myself and
+only copied the code that my professor provided
+to complete my workshops and assignments.
+
+Workshop 3 part 1
+Course title:OOP244 NBB
+Module:      Train
+Filename:    Train.cpp
+Version:     1
+Author:	    Zhaokai Guan
+Student Num: 130988215
+Email:       zguan25@myseneca.ca
+Date:        Feb 2nd 2022
+
+Revision History
+-----------------------------------------------------------
+Date:   Reason:
+-----------------------------------------------------------*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include"Train.h"
+using namespace std;
+
+namespace sdds{
+ 
+    void Train::set(const char* name, int ppl, double speed) {
+       /* Safe and empty defined by Zhaokai :
+
+          string m_Train_Name's first element is null terminator.
+
+          int m_ppl_On_Board equals to -2 ( do not make any sense in reality.
+
+          double m_train_Speed equals to -320.5 ( even when the train is reversing, the speed
+            can not reach that fast)
+       */
+       if ((name && strlen(name))&& (ppl >= 0 && ppl <= MAX_PEOPLE) && (speed >= 0 && speed <= MAX_SPEED)) {
+          strncpy(m_train_Name, name, MAX_NAME_LENGTH);
+          m_ppl_On_Board = ppl;
+          m_train_Speed = speed;
+       }
+       else {
+          m_train_Name[0] = 0;
+          m_ppl_On_Board = -2;
+          m_train_Speed = -320.5;
+       }
+    }
+    
+    int Train::getNumberOfPeople() const{
+       return m_ppl_On_Board;
+    }
+    
+    const char* Train::getName() const{
+       return m_train_Name;
+    }
+    
+    double Train::getSpeed() const{
+       return m_train_Speed;
+    }
+
+    // function actually just checking not setting.//
+    bool Train::isSafeEmpty() const{
+       /* Safe and empty defined by Zhaokai :
+
+          string m_Train_Name's first element is null terminator.
+
+          int m_ppl_On_Board equals to -2 ( do not make any sense in reality.
+
+          double m_train_Speed equals to -320.5 ( even when the train is reversing, the speed
+            can not reach that fast)
+       */
+       return !(m_train_Name[0]) && m_ppl_On_Board == -2 && m_train_Speed == -320.5 ? true : false;
+    }
+    void Train::display() const{
+       if (isSafeEmpty()){
+          cout << "Safe Empty State!" << endl;
+       }
+       else{
+          cout << "NAME OF THE TRAIN : " << m_train_Name << endl;
+          cout << "NUMBER OF PEOPLE  : " << m_ppl_On_Board << endl;
+          cout << "SPEED             : " << m_train_Speed << endl;
+       }
+    }
+}
---
+++ WS03/lab/Train.h
@@ -0,0 +1,62 @@
+/* ------------------------------------------------------
+I have done all the coding by myself and
+only copied the code that my professor provided
+to complete my workshops and assignments.
+
+Workshop 3 part 1
+Course title:OOP244 NBB
+Module:      Train
+Filename:    Train.h
+Version:     1
+Author:	    Zhaokai Guan
+Student Num: 130988215
+Email:       zguan25@myseneca.ca
+Date:        Feb 2nd 2022
+
+Revision History
+-----------------------------------------------------------
+Date:   Reason:
+-----------------------------------------------------------*/
+#ifndef SDDS_TRAIN_H
+#define SDDS_TRAIN_H
+namespace sdds {
+
+	 const int MAX_NAME_LENGTH = 20;
+	 const int MAX_PEOPLE = 1000;
+	 const int MAX_SPEED = 320;
+
+
+	 class Train {
+		  char m_train_Name[MAX_NAME_LENGTH];
+		  int  m_ppl_On_Board;
+		  double  m_train_Speed;
+	 
+
+
+	 public:
+		  // set the train's 3 members up//
+		  void set(const char* train_name, int num_Of_Ppl, double speed);
+		  int getNumberOfPeople() const;
+		  const char* getName() const;
+		  double getSpeed() const;
+		  bool isSafeEmpty() const;
+		  void display() const;
+	 };
+	 
+
+
+
+
+
+	 
+
+	
+	
+	
+
+
+
+
+}
+#endif // !SDDS_TRAIN_H
+
