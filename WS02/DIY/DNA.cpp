e8f2fcb02c35bfbed2e83736a1557b98af160a7a
Zhaokai Guan <zguan25@myseneca.ca>
2022-02-01 20:03:52 -0500

    Finalized and submitted
    Signed-off-by: Zhaokai Guan <zguan25@myseneca.ca>

--- WS02/DIY/DNA.cpp
+++ WS02/DIY/DNA.cpp
@@ -8,7 +8,7 @@ Course title:OOP244 NBB
 Module:      DNA
 Filename:    DNA.cpp
 Version:     1
-Author:	    Zhaokai Guan
+Author:	     Zhaokai Guan
 Student Num: 130988215
 Email:       zguan25@myseneca.ca
 Date:        Jan 29th 2022
@@ -35,6 +35,8 @@ namespace sdds {
     
     bool beginSearch(const char* filename){
         DNA_file_ptr = fopen(filename, "r");
+        matches_Storage = new DNA[storage_Arr_size];
+        cout << "DNA search program" << endl;
         return DNA_file_ptr ? true : false;
     }
 
@@ -44,16 +46,15 @@ namespace sdds {
         storage_Arr_size = 2;
         int temp_DNA_num = 0;
         char temp_String_holder[max_DNA_str_size] = { 0 };
-        matches_Storage = new DNA[storage_Arr_size];
         // Otherwise the search function can be only running once// 
         rewind(DNA_file_ptr);
-        while (fscanf(DNA_file_ptr, "%d,%s\n", &temp_DNA_num,temp_String_holder) != EOF) {
+        while (fscanf(DNA_file_ptr, "%d,%s\n", &temp_DNA_num, temp_String_holder) != EOF) {
             // Calling prof.'s cstrtools function "strStr"//
             // implement DMA for matches storage//
-            if (strStr(temp_String_holder,subDNA)){
-                if (matches_counter==storage_Arr_size){
-                    DNA* temp_holder = new DNA[storage_Arr_size+storage_size_increment];
-                    for (int i = 0; i < storage_Arr_size; i++){
+            if (strStr(temp_String_holder, subDNA)) {
+                if (matches_counter == storage_Arr_size) {
+                    DNA* temp_holder = new DNA[storage_Arr_size + storage_size_increment];
+                    for (int i = 0; i < storage_Arr_size; i++) {
                         temp_holder[i] = matches_Storage[i];
                     }
                     delete[] matches_Storage;
@@ -62,7 +63,7 @@ namespace sdds {
                 }
                 matches_Storage[matches_counter].DNA_Num = temp_DNA_num;
                 // Calling prof.'s cstrtools functions strLen and strCpy//
-                matches_Storage[matches_counter].DNA_String = new char[strLen(temp_String_holder)+1];
+                matches_Storage[matches_counter].DNA_String = new char[strLen(temp_String_holder) + 1];
                 strCpy(matches_Storage[matches_counter].DNA_String, temp_String_holder);
                 matches_counter++;
             }
@@ -70,10 +71,10 @@ namespace sdds {
         return matches_counter ? true : false;
     }
 
+
     void sort() {
         // using the traditional bubble sort learned from last year to acomplish this//
         int i, j;
-        DNA temp = {0};
         // Will not use i to access each element. //
         // i is more like a round counter in order to run enough times for the sorting..//
         for ( i = matches_counter-1; i > 0 ; i--){
@@ -82,9 +83,9 @@ namespace sdds {
                 // if the left element is bigger than the one from right//
                 // shift it to the right by one index//
                 if (matches_Storage[j].DNA_Num > matches_Storage[j + 1].DNA_Num) {
-                    temp.DNA_Num = matches_Storage[j].DNA_Num;
-                    matches_Storage[j].DNA_Num = matches_Storage[j + 1].DNA_Num;
-                    matches_Storage[j + 1].DNA_Num = temp.DNA_Num;
+                    DNA temp= matches_Storage[j];
+                    matches_Storage[j]= matches_Storage[j + 1];
+                    matches_Storage[j + 1]= temp;          
                 }
             }
         }
@@ -92,6 +93,7 @@ namespace sdds {
 
     // Simply display the matches records//
     void displayMatches() {
+        cout << matches_counter << " matches found:" << endl;
         for (int i = 0; i < matches_counter; i++){
             cout << i + 1 << ") " << matches_Storage[i].DNA_Num << ":" << endl;
             cout << matches_Storage[i].DNA_String << endl;
@@ -102,14 +104,14 @@ namespace sdds {
 
     // Not only deallocate the struct arr, but also deallocate each member from each arr element//
     void deallocate() {
-        if (matches_counter){
-            for (int i = 0; i < matches_counter; i++) {
-                delete[] matches_Storage[i].DNA_String;
-                matches_Storage[i].DNA_String = nullptr;
-            }
+        
+         for (int i = 0; i < matches_counter; i++) {
+               delete[] matches_Storage[i].DNA_String;
+               matches_Storage[i].DNA_String = nullptr;
+         }
             delete[] matches_Storage;
             matches_Storage = nullptr;
-        }
+        
     }
 
     // fclose file ptr and set it as nullptr to exit gracefully//
