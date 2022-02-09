a8b873a34afd6cb937778bd873f506e0836780f7
Zhaokai Guan <zguan25@myseneca.ca>
2022-02-09 13:26:17 -0500

    output matched, 32kb memory leak.
    Signed-off-by: Zhaokai Guan <zguan25@myseneca.ca>

--- WS04/lab/CarInventory.cpp
+++ WS04/lab/CarInventory.cpp
@@ -11,14 +11,14 @@ namespace sdds {
 	}
 
 	CarInventory::CarInventory(const char* type, const char* brand, const char* model, int year, int code, double price) {
-		
+
 		bool valid = type && brand && model && year >= 1990 && code >= 100 && code <= 999 && price > 0;
 
-		if (!valid){
+		if (!valid) {
 			resetInfo();
 		}
 
-		else{
+		else {
 			m_brand = new char[strlen(brand) + 1];
 			m_model = new char[strlen(model) + 1];
 			m_type = new char[strlen(type) + 1];
@@ -48,14 +48,14 @@ namespace sdds {
 		m_code = 0;
 	}
 
-	CarInventory& CarInventory:: setInfo(const char* type, const char* brand, const char* model, int year, int code, double price) {
+	CarInventory& CarInventory::setInfo(const char* type, const char* brand, const char* model, int year, int code, double price) {
 		resetInfo();
 		bool valid = type && brand && model && year >= 1990 && code >= 100 && code <= 999 && price > 0;
 		if (!valid) {
 			resetInfo();
 		}
 
-		else{
+		else {
 			m_brand = new char[strlen(brand) + 1];
 			m_model = new char[strlen(model) + 1];
 			m_type = new char[strlen(type) + 1];
@@ -69,16 +69,16 @@ namespace sdds {
 		return *this;
 	}
 
-	void CarInventory::printInfo() const{
-		cout << "|";
+	void CarInventory::printInfo() const {
+		cout << "| ";
 		cout.setf(ios::left);
-		cout.width(12);
+		cout.width(11);
 		cout << m_type;
-		cout << "|";
-		cout.width(18);
+		cout << "| ";
+		cout.width(17);
 		cout << m_brand;
-		cout << "|";
-		cout.width(18);
+		cout << "| ";
+		cout.width(17);
 		cout << m_model;
 		cout << "| ";
 		cout << m_year << " | ";
@@ -95,20 +95,20 @@ namespace sdds {
 		cout.unsetf(ios::fixed);
 	}
 
-	bool CarInventory::isValid() const{
-		return m_type&& m_brand&& m_model&& m_year >= 1990 && m_price > 0 && m_code >= 100 && m_code <= 999;
+	bool CarInventory::isValid() const {
+		return m_type && m_brand && m_model && m_year >= 1990 && m_price > 0 && m_code >= 100 && m_code <= 999;
 	}
 
-	bool CarInventory::isSimilarTo(const CarInventory& car) const{
+	bool CarInventory::isSimilarTo(const CarInventory& car) const {
 		return !strcmp(m_brand, car.m_brand) && !strcmp(m_model, car.m_model) && !strcmp(m_type, car.m_type) && m_year == car.m_year;
 	}
-	
-	bool find_similar(CarInventory car[], const int num_cars){
+
+	bool find_similar(CarInventory car[], const int num_cars) {
 		bool found = false;
 		for (int i = 0; i < num_cars; i++) {
 			for (int j = i + 1; j < num_cars; j++) {
 				if (car[i].isValid() && (car[j]).isValid()) {
-					if (car[i].isSimilarTo(car[j])){
+					if (car[i].isSimilarTo(car[j])) {
 						found = true;
 					}
 				}
