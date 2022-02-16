fec378e9393313972c6f44eae248f36f30d5b460
Zhaokai Guan <zguan25@myseneca.ca>
2022-02-16 14:27:48 -0500

    Eye checked the output matched. Consult prof for overloading the !=. Needs to do -feed.

---
+++ WS05/lab/lab/Flight.cpp
@@ -0,0 +1,254 @@
+/* ------------------------------------------------------
+I have done all the coding by myself and
+only copied the code that my professor provided
+to complete my workshops and assignments.
+
+Workshop 5 part 1
+Course title:OOP244 NBB
+Module:      Flight
+Filename:    Flight.h
+Version:     1
+student:	    Zhaokai Guan
+Student Num: 130988215
+Email:       zguan25@myseneca.ca
+Date:        Feb 9th 2022
+
+Revision History
+-----------------------------------------------------------
+Date:   Reason:
+-----------------------------------------------------------*/
+
+
+// professor's remarks//
+/* ------------------------------------------------------
+Workshop 5 part 1
+Module: Flight
+Filename: Flight.cpp
+Version 1.0
+Date: 24/11/2021
+Author: Asam Gulaid
+Revised by: Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Initials    Date           Reason
+F.S.        07/02/2022     Peer Review
+-----------------------------------------------------------*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include "Flight.h"
+
+using namespace std;
+namespace sdds {
+    void Flight::emptyPlane() {
+        m_passengers = 0;
+        m_fuel = 0;
+        strcpy(m_title , "EmptyPlane");
+    }
+
+    // New Flight
+    Flight::Flight() {
+        emptyPlane();
+    }
+    Flight::Flight(int passengers, double fuel, const char* title) {
+        emptyPlane();
+        if (passengers > 0 && passengers <= Boen747Capacity && fuel >= 0 &&
+            fuel <= FuelTankCapacity) {
+            m_passengers = passengers;
+            m_fuel = fuel;
+            strcpy(m_title,title);
+        }
+    }
+    std::ostream& Flight::display() const {
+
+        if (*this) {
+            cout << " Flight  |  Ready to Depart";
+            cout << " ";
+            cout.width(10);
+            cout << std::string(*this);
+            cout << " | ";
+            cout << "Pass: ";
+            cout.width(10);
+            cout << int(*this);
+            cout << " | ";
+            cout.width(12);
+            cout.precision(2);
+            cout.setf(ios::right);
+            cout.setf(ios::fixed);
+            cout << double(*this);
+            cout.unsetf(ios::right);
+            cout << " Liters";
+        }
+        else if (~*this) {
+            cout << " Flight  |  Empty Plane    ";
+        }
+        else {
+            cout << " Flight  |  Low Fuel       ";
+            cout << " ";
+            cout.width(10);
+            cout << (const char *)(*this);
+            cout << " | ";
+            cout << "Pass: ";
+            cout.width(10);
+            cout << int(*this);
+            cout << " | ";
+            cout.width(12);
+            cout.precision(2);
+            cout.setf(ios::right);
+            cout.setf(ios::fixed);
+            cout << double(*this);
+            cout.unsetf(ios::right);
+            cout << " Liters";
+        }
+        return cout;
+    }
+
+    //The following function or method defintions are made by Zhaokai Guan
+    
+    Flight::operator bool() const{
+       return m_passengers > 0 && m_fuel >= m_passengers * requiredFuelPerPassenger ? true : false;
+    }
+
+    Flight::operator int() const{
+       return m_passengers;
+    }
+
+    Flight::operator double() const{
+       return m_fuel;
+    }
+
+    Flight::operator const char* () const{
+       return m_title;
+    }
+
+    bool Flight::operator~() const{
+       return !m_passengers ? true : false;
+    }
+
+    Flight& Flight::operator=(Flight& right_Operand){
+       strcpy(m_title, right_Operand.m_title);
+       m_passengers = right_Operand.m_passengers;
+       m_fuel = right_Operand.m_fuel;
+       right_Operand.emptyPlane();
+       return *this;
+    }
+
+    Flight& Flight::operator=(int assigning_Guests){
+       if (assigning_Guests && assigning_Guests<=Boen747Capacity){
+          (*this).m_passengers = assigning_Guests;
+       }
+       return *this;
+    }
+
+    Flight& Flight::operator=(double assigning_Fuel){
+       if (assigning_Fuel>0 && assigning_Fuel<FuelTankCapacity){
+          (*this).m_fuel = assigning_Fuel;
+       }
+       return *this;
+    }
+
+    Flight& Flight::operator+=(double adding_Fuel){
+       if (adding_Fuel>0 && m_fuel<FuelTankCapacity){
+          if ((m_fuel+adding_Fuel)<FuelTankCapacity){
+             m_fuel += adding_Fuel;
+          }
+          else{
+             m_fuel = FuelTankCapacity;
+          }
+       }
+    
+       return *this;
+    }
+
+    Flight& Flight::operator+=(int adding_Guests){
+       if (adding_Guests>0 && m_passengers<Boen747Capacity){
+          if ((m_passengers+adding_Guests)<Boen747Capacity){
+             m_passengers += adding_Guests;
+          }
+          else{
+             m_passengers = Boen747Capacity;
+          }
+       }
+       return *this;
+    }
+
+    Flight& Flight::operator-=(double reducing_Fuel){
+       if (reducing_Fuel>0.0){
+          if ((m_fuel-reducing_Fuel)>0.0){
+             m_fuel -= reducing_Fuel;
+          }
+          else{
+             m_fuel = 0.0;
+          }
+       }
+       return *this;
+    }
+
+    Flight& Flight::operator-=(int reducing_Guests){
+       if (reducing_Guests>0){
+          if ((m_passengers - reducing_Guests) > 0) {
+             m_passengers -= reducing_Guests;
+          }
+          else{
+             m_passengers = 0;
+          }
+       }
+       return *this;
+    }
+
+    Flight& Flight::operator<<(Flight& source_Flight) {
+       //overloaded != operator to check if the two flights are the same//
+       if (!( *this != source_Flight)) {
+          if ((m_passengers+source_Flight.m_passengers)<Boen747Capacity){
+             m_passengers += source_Flight.m_passengers;
+             source_Flight.m_passengers = 0;
+          }
+          else{
+             source_Flight.m_passengers -= (Boen747Capacity - m_passengers);
+             m_passengers = Boen747Capacity;             
+          }
+       }
+       return *this;
+    }
+
+    Flight& Flight::operator>>(Flight& destination_Flight){
+       //overloaded != operator to check if the two flights are the same//
+       if (!(*this!=destination_Flight)){
+          if ((destination_Flight.m_passengers+m_passengers)<Boen747Capacity){
+             destination_Flight.m_passengers += m_passengers;
+             m_passengers = 0;
+          }
+          else{
+             m_passengers -= (Boen747Capacity - destination_Flight.m_passengers);
+             destination_Flight = Boen747Capacity;
+          }
+
+       }
+       return *this;
+    }
+
+    // build to increase the readbility and avoid the repeatition//
+    // Otherwise there will be a super long condition inside << and >> methods//
+    bool Flight::operator!=(Flight& second_Flight)const{
+       bool result = false;
+       if (!(strcmp(m_title, second_Flight.m_title))){
+          if (m_passengers==second_Flight.m_passengers){
+             if (m_fuel==second_Flight.m_fuel)
+             {
+                result = true;
+             }
+          }
+       }
+       return result;
+    }
+    
+    // binary helper operators//
+    int operator+(const Flight& first_Flight, const Flight& second_Flight){
+       int result;
+       return result = (bool(first_Flight) && bool(second_Flight)) ? int(first_Flight) + int(second_Flight) : 0;
+    }
+
+    int operator+=(int& passenger_Sum, Flight& source_Flight){
+       return passenger_Sum += int(source_Flight);
+    }
+
+}
---
+++ WS05/lab/lab/Flight.h
@@ -0,0 +1,94 @@
+/* ------------------------------------------------------
+I have done all the coding by myself and
+only copied the code that my professor provided
+to complete my workshops and assignments.
+
+Workshop 5 part 1
+Course title:OOP244 NBB
+Module:      Flight
+Filename:    Flight.h
+Version:     1
+student:	    Zhaokai Guan
+Student Num: 130988215
+Email:       zguan25@myseneca.ca
+Date:        Feb 9th 2022
+
+Revision History
+-----------------------------------------------------------
+Date:   Reason:
+-----------------------------------------------------------*/
+
+//professor's remarks//
+
+/* ------------------------------------------------------
+Workshop 5 part 1
+Module: Flight
+Filename: Flight.h
+Version 1.0
+Date: 24/11/2021
+Author: Asam Gulaid
+Revised by: Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Initials    Date           Reason
+F.S.        07/02/2022     Peer Review
+-----------------------------------------------------------*/
+
+
+
+
+#ifndef SDDS_FLIGHT_H
+#define SDDS_FLIGHT_H
+#include <iostream>
+
+namespace sdds {
+
+   const int Boen747Capacity = 388;
+   const int FuelTankCapacity = 238840;
+
+   //This variable is created by student//
+   const int requiredFuelPerPassenger = 600;
+
+   class Flight {
+
+      int m_passengers = 0;
+      double m_fuel = 0;
+      char m_title[16];
+      void emptyPlane();
+
+   public:
+      Flight();
+      Flight(int passengers, double fuel, const char* title);
+      std::ostream& display()const;
+      
+      //the following parts are done by Zhaokai Guan//
+      
+      //type conversion operators//
+      operator bool()const;
+      operator int()const;
+      operator double()const;
+      operator const char*()const;
+
+      //unary member operator//
+      bool operator ~()const;
+     
+      //binary member operator//
+      Flight& operator =(Flight& right_Operand);
+      Flight& operator =(int assigning_Guests);
+      Flight& operator =(double assigning_Fuel);
+      Flight& operator +=(double adding_Fuel);
+      Flight& operator +=(int adding_Guests);
+      Flight& operator -=(double reducing_Fuel);
+      Flight& operator -=(int reducing_Guests);
+      Flight& operator <<(Flight& source_Flight);
+      Flight& operator >>(Flight& destination_flight);
+      bool operator !=(Flight& second_flight)const;
+
+   };
+
+   //binary helper operator//
+   int operator + (const Flight& first_Flight, const Flight& second_Flight);
+   int operator += (int& passenger_Sum, Flight& source_Flight);
+}
+
+#endif // SDDS_FLIGHT_H
