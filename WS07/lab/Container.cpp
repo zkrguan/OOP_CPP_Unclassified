f7769f4590e088ca22db3698ab54662390a03648
Zhaokai Guan <zguan25@myseneca.ca>
2022-03-15 23:18:40 -0400

    Finished Building Container.cpp

--- WS07/lab/Container.cpp
+++ WS07/lab/Container.cpp
@@ -3,12 +3,128 @@
 #include "Container.h"
 
 namespace sdds {
-   container::container()
-   {
-
+   container::container(const char* srcContent, int srcCapacity){
+      if (!srcContent){
+         m_capacity = 0;
+      }
+      else{
+         strcpy(m_content, srcContent);
+         m_capacity = srcCapacity;
+      }
+   }
+   container::container(const char* srcContent, int srcCapacity, int srcContentVolume){
+      if (!srcContent||srcContentVolume>srcCapacity){
+         //To set invalid//
+      }
+      else{
+         m_capacity = srcCapacity;
+         m_contentVolume = srcContentVolume;
+         strcpy(m_content, srcContent);
+      }
    }
 
-   container::~container()
-   {
+   container::~container(){
+
    }
+   
+   container& container::setEmpty() {
+      // invalid state //
+      m_capacity = -1;
+      m_contentVolume = -2;
+   }
+
+   int container::capacity() const{
+      return m_capacity;
+   }
+
+   int container::volume() const{
+      return m_contentVolume;
+   }
+
+   int container::operator+=(int srcVolume){
+      int result = 0;
+      if (m_contentVolume!=m_capacity){
+         if ((srcVolume+m_contentVolume)<=m_capacity){
+            m_contentVolume += srcVolume;
+            result = srcVolume;
+         }
+         else{
+            result = srcVolume - m_capacity;
+            m_contentVolume += result;
+         }
+      }
+      return result;
+   }
+
+   int container::operator-=(int deductVolume){
+      int result = 0;
+      if (m_contentVolume){
+         if ((m_contentVolume-deductVolume)>=0){
+            m_contentVolume -= deductVolume;
+            result = deductVolume;
+         }
+         else{
+            result = m_contentVolume;
+            m_contentVolume = 0;
+         }
+      }
+      return result;
+   }
+
+   container::operator bool() const{
+      return (m_contentVolume != -2 || m_capacity != -1);
+   }
+
+   void container::clear(const int capacity, const char* content) {
+      if (capacity && content){
+         strcpy(m_content, content);
+         m_capacity = capacity;
+      }
+   }
+
+   std::ostream& container::print(std::ostream& ostr) const{
+      if (this->operator bool()){
+         ostr << m_content << ": (" << m_contentVolume << "cc/" << m_capacity << ")" << std::endl;
+         return ostr;
+      }
+      else{
+         ostr << "****: (**cc/***)" << std::endl;
+      }
+   }
+
+   std::istream& container::read(std::istream& istr){
+      
+         int input = 0;
+         bool flag = true;
+         
+         do {
+            if (!bool(*this)){
+               std::cout << "Broken Container, adding aborted! Press <ENTER> to continue...." << std::endl;
+               istr.get();
+            }
+            else
+            {
+               print(std::cout);
+               std::cout << ">";
+               if (!(istr >> input)) {
+                  std::cout << "Invalid Integer, retry: ";
+                  istr.clear();
+                  istr.ignore(1000, '\n');
+               }
+               else{
+                  if (input<1||input>999){
+                     std::cout << "Value out of range [1<=val<=999]: ";
+                  }
+                  else {
+                     std::cout << "Added " << ((*this) += input) << "CCs" << std::endl;
+                     flag = false;
+                  }
+               }             
+            }  
+         } while (flag);
+   }
+
+
+
+
 }
--- WS07/lab/Container.h
+++ WS07/lab/Container.h
@@ -2,24 +2,23 @@
 #define SDDS_CONTAINER_H
 namespace sdds {
 	class container{
-		char m_content[50];
-		int m_capacity;
-		int m_contentVolume;
+		char m_content[50]{};
+		int m_capacity{};
+		int m_contentVolume{};
 	public:
-		container();
 		container(const char*srcContent, int srcCapacity);
 		container(const char*srcContent, int srcCapacity, int srcContentVolume);
 		~container();
 	protected:
-		container& setEmpty() const;
-		container& capacity() const;
-		container& volume() const;
+		container& setEmpty() ;
+		int capacity() const;
+		int volume() const;
 	public:
 		int operator += (int srcVolume);
 		int operator -=(int dstVolume);
 		operator bool()const;
 		// Temp spot for the container methods// 
-		void clear(const int capacity, const char* content) const;
+		void clear(const int capacity, const char* content);
 		std::ostream& print(std::ostream& ostr) const;
 		std::istream& read(std::istream& istr);
 	};
